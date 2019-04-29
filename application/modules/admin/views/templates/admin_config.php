eccb62dec0c3e684689474e40da7ef00daee8aed
Franz Wilder Gualambo Giraldo <gualambo@gmail.com>
2019-04-29 11:43:31 -0500

    f

--- application/modules/admin/views/templates/admin_config.php
+++ application/modules/admin/views/templates/admin_config.php
@@ -17,6 +17,7 @@
 		<![endif]-->
 
     <script type="text/javascript" src="static/main/jquery.js"> </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script type="text/javascript" src="static/main/bootstrap/js/bootstrap.min.js"></script>
     <link rel="icon" href="osinerg.ico" type="image/ico">
 
