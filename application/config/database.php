2df682bc11900b57a1226d6aa4177eff75a49e32
Franz Wilder Gualambo Giraldo <gualambo@gmail.com>
2019-05-08 22:31:04 -0500

    d

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
+	'hostname' => 'localhost', 
 	'username' => 'jmcracin_user',
 	'password' => '*JMC123*',
 	'database' => 'jmcracin_jmc',
